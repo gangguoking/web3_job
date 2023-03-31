a419f1dbe164f909e486264f17f01bf7afd7f808
gangguoking <huhao13053282925@gmail.com>
2023-03-31 21:28:15 +0800

    update web3_career.py add write json

--- web3_job/spiders/web3_career.py
+++ web3_job/spiders/web3_career.py
@@ -3,7 +3,14 @@ import json
 import scrapy
 
 
-MAX_PAGE = 3
+MAX_PAGE = 7
+
+COMPANY_LIST = {
+  "Kodex": "james@kodex.io",
+  "GammaSwap Labs": "dgoodkin@gammaswap.com",
+  "Lemon.io": "https://lemon.io/escape-the-matrix/",
+  "Etherscan": "jobs@etherscan.io"
+}
 
 
 class Web3CareerSpider(scrapy.Spider):
@@ -13,12 +20,22 @@ class Web3CareerSpider(scrapy.Spider):
 
     def parse(self, response):
         post_jd_xpath_list = response.xpath('//script[@type="application/ld+json"]')
-        post_jd_list = []
+        if "post_jd_list" in response.meta:
+            post_jd_list = response.meta['post_jd_list']
+        else:
+            post_jd_list = []
         for row in post_jd_xpath_list:
             json_data = json.loads(row.root.text)
             if json_data['@type'] == 'JobPosting':
+                company_name = json_data['hiringOrganization']['name']
+                if company_name in COMPANY_LIST:
+                    # print(json_data['hiringOrganization']['name'])
+                    json_data['apply'] = COMPANY_LIST[company_name]
+                else:
+                    continue
+                print(json_data)
                 post_jd_list.append(json_data)
-            yield json_data
+            # yield json_data
 
         with open('data.json', 'w', encoding='utf-8') as fp:
             json.dump({'post_jd_list': post_jd_list}, fp)
@@ -33,7 +50,8 @@ class Web3CareerSpider(scrapy.Spider):
 
         yield scrapy.Request(url=next_url,
                              dont_filter=True,
-                             callback=self.parse)
+                             callback=self.parse,
+                             meta={"post_jd_list": post_jd_list})
 
 
 # use scrapy，local
